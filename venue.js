ff41e6c9ab372c7ffe19237b39d269a7ed091458
jimbog <jimmygarzon@gmail.com>
2015-02-17 17:32:38 -0800

    add rooms

--- venue.js
+++ venue.js
@@ -1,12 +1,12 @@
 Messages = new Meteor.Collection('messages');
+Rooms = new Meteor.Collection('rooms');
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('currentRoom', '');
 
   Template.chat.helpers({
     messageList: function () {
-      return Messages.find({roomName: 'myRoomster'});
+      return Messages.find({roomName: Session.get('currentRoom') });
     }
   });
 
@@ -15,8 +15,29 @@ if (Meteor.isClient) {
       var message = $('#messageBox').val();
       var roomName = $('#roomBox').val();
 
-      Messages.insert({content: message, roomName: roomName, createdAt: new Date()});
+      Messages.insert({content: message, roomName: Session.get('currentRoom'), postedBy: Meteor.user(), createdAt: new Date()});
       $('#messageBox').val('');
+    }
+  });
+
+  Template.rooms.helpers({
+    roomList: function () {
+      console.log(Rooms.find().fetch());
+      return Rooms.find();
+    }
+  });
+
+  Template.rooms.events({
+    'click #createRoom': function () {
+      var roomName = $('#roomBox').val();
+
+      Rooms.insert({name: roomName, createdAt: new Date()});
+      $('#roomBox').val('');
+    },
+    'change select': function(){
+      var currentRoom = $('select').val();
+      console.log(currentRoom);
+      Session.set('currentRoom', currentRoom);
     }
   });
 }
